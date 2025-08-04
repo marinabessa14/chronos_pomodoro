fc7272f28a11b6b02fc644b6d5bdf602472c0f67
marinabessa <marinabessarodrigues@hotmail.com>
2025-08-04 20:48:51 -0300

    chronos

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.css';
+import { RouterLink } from '../RouterLink';
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <RouterLink href='/about-pomodoro/'>
+        Entenda como funciona a técnica pomodoro
+      </RouterLink>
+      <RouterLink href='/'>
+        Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito por - Marina Bessa
+      </RouterLink>
+    </footer>
+  );
+}
